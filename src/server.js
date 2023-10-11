014b9ea0fcc5d7e12353eb9dfa91085ad83a329d
MohammadDaadour <daadourmohammad@gmail.com>
2023-10-12 01:27:06 +0300

    m

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://mwz3.com/api/v2";
-export const backend_url = "https://mwz3.com/";
+export const server = "https://www.mwz3.com/api/v2";
+export const backend_url = "https://www.mwz3.com/";
 
 
