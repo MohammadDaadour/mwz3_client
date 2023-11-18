12c731d86ff40d73ab4370576651ce341d00e6fd
MohammadDaadour <daadourmohammad@gmail.com>
2023-11-19 00:09:25 +0200

    m

--- src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -4,32 +4,22 @@ import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
 
 const FeaturedProduct = () => {
-  const { allProducts } = useSelector((state) => state.products);
-
-  const electronicProducts = allProducts.filter(
-    (product) => product.category === "أجهزة الكترونية"
-  );
-
-  const display = (category) => {
-    const filteredProducts = allProducts.filter(
-      (product) => product.category === category
-    );
-
-    const displayedProducts = filteredProducts.slice(0, 10);
-    return displayedProducts;
-  };
-
+  const {allProducts} = useSelector((state) => state.products);
+   
   return (
     <div>
       <div className={`${styles.section} py-16 px-8`}>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-          {allProducts &&
-            display("أجهزة كهربائية").length !== 0 &&
-            display("أجهزة كهربائية") && (
-              <div className={`${styles.heading}`}>
-                <h1 lang="ar">أجهزة كهربائية</h1>
-              </div>
-            )}
+        <div className={`${styles.heading}`}>
+          <h1 lang="ar">منتجات رائجة</h1>
+        </div>
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">z
+        {
+            allProducts && allProducts.length !== 0 &&(
+              <>
+               {allProducts && allProducts.map((i, index) => <ProductCard data={i} key={index} />)}
+              </>
+            )
+           }
         </div>
       </div>
     </div>
@@ -37,3 +27,41 @@ const FeaturedProduct = () => {
 };
 
 export default FeaturedProduct;
+
+// import React, { useEffect } from "react";
+// import { useSelector } from "react-redux";
+// import styles from "../../../styles/styles";
+// import ProductCard from "../ProductCard/ProductCard";
+
+// const FeaturedProduct = () => {
+//   const { allProducts } = useSelector((state) => state.products);
+
+//   const display = (category) => {
+//     if (allProducts) {
+//       const filteredProducts = allProducts.filter(
+//         (product) => product.category === category
+//       );
+
+//       const displayedProducts = filteredProducts.slice(0, 10);
+//       return displayedProducts;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div className={`${styles.section} py-16 px-8`}>
+//         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+//           {allProducts &&
+//             display("أجهزة كهربائية").length !== 0 &&
+//             display("أجهزة كهربائية") && (
+//               <div className={`${styles.heading}`}>
+//                 <h1 lang="ar">أجهزة كهربائية</h1>
+//               </div>
+//             )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FeaturedProduct;
