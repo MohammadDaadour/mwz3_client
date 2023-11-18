cfaa49a60de24790005283d0c2275521c41fc884
MohammadDaadour <daadourmohammad@gmail.com>
2023-11-18 23:55:11 +0200

    m

--- src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -4,22 +4,32 @@ import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
 
 const FeaturedProduct = () => {
-  const {allProducts} = useSelector((state) => state.products);
-   
+  const { allProducts } = useSelector((state) => state.products);
+
+  const electronicProducts = allProducts.filter(
+    (product) => product.category === "أجهزة الكترونية"
+  );
+
+  const display = (category) => {
+    const filteredProducts = allProducts.filter(
+      (product) => product.category === category
+    );
+
+    const displayedProducts = filteredProducts.slice(0, 10);
+    return displayedProducts;
+  };
+
   return (
     <div>
       <div className={`${styles.section} py-16 px-8`}>
-        <div className={`${styles.heading}`}>
-          <h1 lang="ar">منتجات رائجة</h1>
-        </div>
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-        {
-            allProducts && allProducts.length !== 0 &&(
-              <>
-               {allProducts && allProducts.map((i, index) => <ProductCard data={i} key={index} />)}
-              </>
-            )
-           }
+          {allProducts &&
+            display("أجهزة كهربائية").length !== 0 &&
+            display("أجهزة كهربائية") && (
+              <div className={`${styles.heading}`}>
+                <h1 lang="ar">أجهزة كهربائية</h1>
+              </div>
+            )}
         </div>
       </div>
     </div>
