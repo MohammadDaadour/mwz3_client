ecdcd7bf96a8d4673df9212855e2f2f83200bc60
MohammadDaadour <daadourmohammad@gmail.com>
2023-11-19 21:57:06 +0200

    m

--- src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -1,25 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
 
 const FeaturedProduct = () => {
-  const {allProducts} = useSelector((state) => state.products);
-   
+  const { allProducts } = useSelector((state) => state.products);
+  if (allProducts) {
+    var elec = allProducts.filter(
+      (product) => product.category === "أجهزة كهربائية"
+    );
+    var disElec = elec.slice(0, 5);
+    var lab = allProducts.filter(
+      (product) => product.category === "أجهزة كمبيوتر و لابتوب"
+    );
+    var disLab = lab.slice(0, 5);
+  }
   return (
     <div>
       <div className={`${styles.section} py-16 px-8`}>
-        <div className={`${styles.heading}`}>
-          <h1 lang="ar">منتجات رائجة</h1>
+        {disElec && <div className={`${styles.heading}`}>
+          <h1 lang="ar">أجهزة كهربائية</h1>
+        </div>}
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+          {allProducts && allProducts.length !== 0 && (
+            <>
+              {disElec &&
+                disElec !== 0 &&
+                disElec.map((i, index) => <ProductCard data={i} key={index} />)}
+            </>
+          )}
         </div>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">z
-        {
-            allProducts && allProducts.length !== 0 &&(
-              <>
-               {allProducts && allProducts.map((i, index) => <ProductCard data={i} key={index} />)}
-              </>
-            )
-           }
+        {disLab && <div className={`${styles.heading}`}>
+          <h1 lang="ar">أجهزة كمبيوتر و لابتوب</h1>
+        </div>}
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
+          {allProducts && allProducts.length !== 0 && (
+            <>
+              {disLab &&
+                disLab !== 0 &&
+                disLab.map((i, index) => <ProductCard data={i} key={index} />)}
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -27,41 +48,3 @@ const FeaturedProduct = () => {
 };
 
 export default FeaturedProduct;
-
-// import React, { useEffect } from "react";
-// import { useSelector } from "react-redux";
-// import styles from "../../../styles/styles";
-// import ProductCard from "../ProductCard/ProductCard";
-
-// const FeaturedProduct = () => {
-//   const { allProducts } = useSelector((state) => state.products);
-
-//   const display = (category) => {
-//     if (allProducts) {
-//       const filteredProducts = allProducts.filter(
-//         (product) => product.category === category
-//       );
-
-//       const displayedProducts = filteredProducts.slice(0, 10);
-//       return displayedProducts;
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div className={`${styles.section} py-16 px-8`}>
-//         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-//           {allProducts &&
-//             display("أجهزة كهربائية").length !== 0 &&
-//             display("أجهزة كهربائية") && (
-//               <div className={`${styles.heading}`}>
-//                 <h1 lang="ar">أجهزة كهربائية</h1>
-//               </div>
-//             )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FeaturedProduct;
