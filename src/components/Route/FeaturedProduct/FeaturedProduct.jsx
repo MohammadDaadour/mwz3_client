c94940d254b6dc02d67000fe4efc6cef3f88793d
MohammadDaadour <daadourmohammad@gmail.com>
2024-01-24 19:55:02 +0200

    m

--- src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -77,6 +77,11 @@ const FeaturedProduct = () => {
             </>
           )}
         </div>
+        {disOther && (
+          <div className={`${styles.heading}`}>
+            <h1 lang="ar">أقسام أخري</h1>
+          </div>
+        )}
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
           {allProducts && allProducts.length !== 0 && (
             <>
