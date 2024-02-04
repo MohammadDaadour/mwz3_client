10f3d4d4893b3bf7d6074cb19297bd054b1141c8
MohammadDaadour <112201830+MohammadDaadour@users.noreply.github.com>
2024-02-04 21:20:40 +0200

    Update AdminModProtectedRoute.js

--- src/routes/AdminModProtectedRoute.js
+++ src/routes/AdminModProtectedRoute.js
@@ -1,1 +1,18 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
+const ProtectedAdminRoute = ({ children }) => {
+  const { loading, isAuthenticated,user } = useSelector((state) => state.user);
+  if (loading === false) {
+    if (!isAuthenticated) {
+      return <Navigate to="/login" replace />;
+    } else if(user.role !== "Admin" && user.role !== "Moderator"){
+        return <Navigate to="/" replace />;
+    }
+    return children;
+  }else {
+    return null;
+  }
+};
+
+export default ProtectedAdminRoute;
