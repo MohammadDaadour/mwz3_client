6c7c3233587a02ee8da01a13217134ef4bcdee4c
MohammadDaadour <112201830+MohammadDaadour@users.noreply.github.com>
2024-02-26 18:09:44 +0200

    Update ShopCreate.jsx

--- src/components/Shop/ShopCreate.jsx
+++ src/components/Shop/ShopCreate.jsx
@@ -144,7 +144,7 @@ const ShopCreate = () => {
               </div>
             </div>
 
-            <div>
+{/*             <div>
               <label
                 htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
@@ -160,7 +160,7 @@ const ShopCreate = () => {
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
               </div>
-            </div>
+            </div> */}
 
             <div>
               <label
